c894d944a33b224fb14ef98c6c6148273c134235
one-v-one <onevonesmtp@gmail.com>
2025-06-05 13:02:18 -0400

    Update index.js

--- src/Constants/index.js
+++ src/Constants/index.js
@@ -1,12 +1,12 @@
 export const CONSTANTS = {
-    // BASE_URL_SOCKET: "http://localhost:5000",
-    // BASE_URL: "http://localhost:5000/api/",
-    BASE_URL_SOCKET: "https://onevone-backend-90vx.onrender.com",
-    BASE_URL: "https://onevone-backend-90vx.onrender.com",
-    LOGIN: "auth/login",
-    REGISTER: "auth/register",
-    VERIFY_EMAIL: "auth/verify-email",
-    RESET_PASSWORD: "auth/reset-password",
-    UPDATE_PASSWORD: "auth/update-password",
-    GET_USER: "users",
-}
+  // BASE_URL_SOCKET: "http://localhost:5000",
+  // BASE_URL:        "http://localhost:5000/api/",
+  BASE_URL_SOCKET: "https://onevone-backend-90vx.onrender.com",
+  BASE_URL:        "https://onevone-backend-90vx.onrender.com/api/",
+  LOGIN:           "auth/login",
+  REGISTER:        "auth/register",
+  VERIFY_EMAIL:    "auth/verify-email",
+  RESET_PASSWORD:  "auth/reset-password",
+  UPDATE_PASSWORD: "auth/update-password",
+  GET_USER:        "users",
+};
